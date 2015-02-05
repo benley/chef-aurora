db3faf1cb983c50141be85d117fb60be2557f4dd
Benjamin Staffin <ben@folsomlabs.com>
2015-02-04 17:01:13 -0800

    Release version 0.1.9
    Change-Id: I41527d19e816c7e9c031411eb0f6805f91bea4d3

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ben@folsomlabs.com'
 license          'Apache 2.0'
 description      'Apache Aurora scheduler and slave nodes'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.8'
+version          '0.1.9'
 
 supports 'ubuntu'
 depends 'apt'
