835586c0c6eab0488f8c539419f7fac6b220e603
Florent Flament <f.flament@criteo.com>
2016-04-22 19:14:33 +0200

    Add Aurora slave support for CentOS 7

--- test/integration/slave/serverspec/thermos_spec.rb
+++ test/integration/slave/serverspec/thermos_spec.rb
@@ -5,7 +5,9 @@ set :backend, :exec
 describe 'Thermos observer' do
 
   it 'is running' do
-    expect(service 'thermos').to be_running
+    sv_name = 'thermos'
+    if os[:family] == 'redhat' then sv_name = 'thermos-observer' end
+    expect(service sv_name).to be_running
   end
 
   it 'is listening on tcp/1338' do
